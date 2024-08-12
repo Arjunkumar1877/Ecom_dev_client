ef8a251cad7de31b403ce3248b17c8c0f139a991
Nihal Mohamed Bashir <nihalmb23@gmail.com>
2024-08-12 17:02:27 +0530

    tailwind installed and configured

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +11,9 @@ function App() {
  <>
  
  <h1>Amal Modesty</h1>
+ <h1 className="text-3xl font-bold underline text-red-700" >
+      Hello world!
+    </h1>
  </>
   )
 }
